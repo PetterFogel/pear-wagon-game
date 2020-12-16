0b673924a1ae77cac41e992e8e1de36f5c5d6999
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2020-12-16 15:36:06 +0100

    keyboard arrows

--- src/howToPlayScreen.ts
+++ src/howToPlayScreen.ts
@@ -10,12 +10,26 @@ class HowToPlayScreen {
         // }
     }
     draw() {
-        clear();
+        // clear();
         background(150, 0, 0);
         imageMode(CENTER)
-        image(img, innerWidth / 2, innerHeight / 2 -200, 800, 500);
+        image(img, innerWidth / 2, innerHeight / 2 -220, 800, 500);
         imageMode(CORNER)
-        image(img2, innerWidth / 2, innerHeight / 2, 300, 400);
+        image(img2, innerWidth / 2 + 200, innerHeight / 2, 300, 400);
+        strokeWeight(2);
+        line(innerWidth / 2 + 220, innerHeight / 2 -100, innerWidth / 2 + 220, innerHeight / 2 - 35);
+        line(innerWidth / 2 + 310, innerHeight / 2 -100, innerWidth / 2 + 310, innerHeight / 2 + 10);
+
+        textSize(22);
+        text('Move left', innerWidth / 2 + 170, innerHeight / 2 - 15)
+        fill(220);
+        text('Move right', innerWidth / 2 + 260, innerHeight / 2 + 30)
+        fill(220);
+
+
+        // line(1000, 800, 1200, 700);
+
+        // line(900, 400, 50, 50);
         
         // this.menuButton.draw(); 
 

e8288ef1283225c6d1b0be8b65cbfa3488dbbfbb
Petter <petter.fogel@medieinstitutet.se>
2021-01-11 10:30:11 +0100

    Add some logic for steering pear wagon.

--- src/player.ts
+++ src/player.ts
@@ -3,20 +3,57 @@ class Player {
     public y: number
     public width: number
     public height: number
+    private offset: number
+    private fallback: number
 
 
     constructor() {
-        this.x = mouseX;
+        this.x = innerWidth / 2;
         this.y = innerHeight - 138;
         this.width = 150;
         this.height = 80;
+        this.offset =  0;
+        this.fallback = 0;
 
     }
 
     update() {
-        this.x = mouseX;
         this.y = innerHeight - 138;
+
+        if(keyIsDown(LEFT_ARROW)) {
+            this.x = this.x - 10;
+            this.offset = this.x;
+        } else if (keyIsDown(RIGHT_ARROW)) {
+            this.x = this.x + 10;
+            this.offset = this.x;
+        } else {
+            this.x = mouseX + this.offset;
+        }
+
+        // if (this.x >= innerWidth) {
+        //     this.x = innerWidth - 75;
+        //     this.offset = this.x;
+        // } else if (this.x <= 0) {
+        //     this.x = innerWidth + 75;
+        //     this.offset = this.x;
+        // }
     }
+    
+    // steering(event: KeyboardEvent) {
+    //     this.x = innerWidth / 2;
+    //     switch (event.key) {
+    //         case "ArrowLeft":
+    //             this.x = this.x - 10;
+    //             console.log("Left");
+    //             console.log(this.x);
+    //             break;
+    //         case "ArrowRight":
+
+    //             console.log("Right")
+    //             console.log(this.x);
+    //             break;
+    //     }
+    // }
 
     draw() {
         push();
@@ -33,14 +70,7 @@ class Player {
         
         image(images.wagon, this.x, this.y, this.width, this.height);
 
-        noCursor();
         pop();
 
     }
-
-
-
-
-
 }
-
